310f30ce9261341cea9f83667c654377a8ca02b7
Amritpal-01 <Amritpalonly13571@gmail.com>
2025-05-13 20:55:59 +0530

    delete loading added (Admin portal)

--- app/contacts/page.js
+++ app/contacts/page.js
@@ -93,7 +93,7 @@ const Page = () => {
         </button>
       </div>
       {isLoadingMessages && (
-        <div role="status" className=" fixed left-[48%] top-[46%]">
+        <div role="status" className=" fixed  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
           <svg
             aria-hidden="true"
             className="w-12 h-12 text-gray-200 animate-spin dark:text-gray-600 fill-black"
